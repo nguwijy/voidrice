d86ebc37acb913e1f7ec8db0ae94e690f4d6ed86
nguwijy <nguwijy@hotmail.com>
2022-06-22 19:23:35 +0800

    feat: add qmk config

---
+++ .config/qmk/nguwijy/config.h
@@ -0,0 +1,13 @@
+/* miliseconds for a key to become mod (used in tapping mode) */
+/* #define TAPPING_TERM 200 */
+
+/* tapping instead of holding when no additional keystroke is pressed */
+#define RETRO_TAPPING
+
+/* by default, holding the same key after tapping it is interpreted as tapping */
+/* such behavior is turned off by defining TAPPING_FORCE_HOLD */
+#define TAPPING_FORCE_HOLD
+
+/* holding whenever another keystroke is pressed */
+/* even if both actions are done within TAPPING_TERM */
+/* #define PERMISSIVE_HOLD */
