bd4f3ada995a392b79c3cc664fc43134fe7e30b0
nguwijy <nguwijy@hotmail.com>
2022-06-27 16:51:02 +0800

    feat: latest nvim configuration using nvchad

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,12 +1,15 @@
 -- n, v, i, t = mode names
-
 local M = {}
 
 M.general = {
 
    n = {
-      -- save and quit all
-      ["<leader>qa"] = { "<cmd> wqa <CR>", "﬚  save file and quit all" },
+      ["<leader>qq"] = { "<cmd> wqa <CR>", "﬚  save file and quit all" },
+      ["<leader>lo"] = { "<cmd> !opout <c-r>%<CR><CR>", "  open preview files" },
+      ["<leader>lb"] = { "<cmd> vsp<space>$BIB<CR>", "  open bibliography file in split mode" },
+      ["<leader>ww"] = { "<cmd> execute 'silent! write !sudo tee % >/dev/null' <bar> edit! <CR>", "﬚  save file with sudo privillege" },
+      ["<leader>cd"] = { "<cmd> cd %:p:h <CR>", "  cd to the directory of current file" },
+      ["<CR>"] = { "<cmd> VimwikiFollowLink <CR>", "  follow/create wiki link" },
    },
 }
 
@@ -14,7 +17,7 @@ M.tabufline = {
 
    n = {
       -- close buffer + hide terminal buffer
-      ["<leader>qq"] = {
+      ["<C-x>"] = {
          function()
             require("core.utils").close_buffer()
          end,
@@ -23,60 +26,49 @@ M.tabufline = {
    },
 }
 
-M.nvimtree = {
+M.lspconfig = {
+   n = {
+      ["<leader>lf"] = { "<cmd> TexlabForward <CR>", "   texlab forward search" },
+   },
+}
 
+M.nvimtree = {
    n = {
       -- toggle
       ["<leader>n"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
    },
 }
 
+M.telescope = {
+   n = {
+      -- git
+      ["<leader>gts"] = { "<cmd> Telescope git_stash <CR>", "   git check stash" },
+      ["<leader>gtc"] = { "<cmd> Telescope git_commits <CR>", "   git check commits" },
+      ["<leader>gtd"] = { "<cmd> Telescope git_status <CR>", "  git check diff" },
+      ["<leader>gtp"] = { "<cmd> Gitsigns preview_hunk <CR>", "  git preview diff of this hunk" },
+      ["<leader>gt<TAB>"] = { "<cmd> Gitsigns next_hunk <CR>", "  git go to next hunk" },
+      ["<leader>gt<S-Tab>"] = { "<cmd> Gitsigns prev_hunk <CR>", "  git go to previous hunk" },
+   },
+}
+
 M.nvterm = {
    t = {
       -- toggle in terminal mode
-      ["<leader>tt"] = {
-         function()
-            require("nvterm.terminal").toggle "float"
-         end,
-         "   toggle floating term",
-      },
-
-      ["<leader>th"] = {
+      ["<C-t>"] = {
          function()
             require("nvterm.terminal").toggle "horizontal"
          end,
          "   toggle horizontal term",
-      },
-
-      ["<leader>tv"] = {
-         function()
-            require("nvterm.terminal").toggle "vertical"
-         end,
-         "   toggle vertical term",
       },
    },
 
    n = {
       -- toggle in normal mode
-      ["<leader>tt"] = {
-         function()
-            require("nvterm.terminal").toggle "float"
-         end,
-         "   toggle floating term",
-      },
-
-      ["<leader>th"] = {
+      ["<C-t>"] = {
          function()
             require("nvterm.terminal").toggle "horizontal"
          end,
          "   toggle horizontal term",
-      },
-
-      ["<leader>tv"] = {
-         function()
-            require("nvterm.terminal").toggle "vertical"
-         end,
-         "   toggle vertical term",
       },
    },
 }
--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -1,4 +1,8 @@
 local opt = vim.opt
+local g = vim.g
+
+-- set local leader
+g.maplocalleader = " "
 
 -- Numbers
 opt.relativenumber = true
@@ -6,3 +10,32 @@ opt.relativenumber = true
 -- Indenting
 opt.shiftwidth = 4
 opt.tabstop = 4
+
+-- vimwiki
+g.vimwiki_list = {
+    {
+        path = '~/OneDrive/quick-drive/vimwiki',
+        syntax = 'markdown',
+        list_margin = 0,
+        ext = '.md'
+    }
+}
+g.vimwiki_ext2syntax = {
+    ['.Rmd'] = 'markdown',
+    ['.rmd'] = 'markdown',
+    ['.md'] = 'markdown',
+    ['.markdown'] = 'markdown',
+    ['.mdown'] = 'markdown'
+}
+g.vimwiki_markdown_link_ext = 1
+g.vimwiki_key_mappings = { ['links'] = 0 }
+
+-- vimwiki
+g.vimtex_delim_toggle_mod_list = {
+    {'\\left', '\\right'},
+    {'\\bigl', '\\bigr'},
+    {'\\Bigl', '\\Bigr'},
+    {'\\biggl', '\\biggr'},
+    {'\\Biggl', '\\Biggr'},
+}
+g.vimtex_view_method = 'zathura'
