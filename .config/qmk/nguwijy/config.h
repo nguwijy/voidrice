47e9af0a9360b40ef98585f952fc257acb7450bf
nguwijy <nguwijy@hotmail.com>
2022-11-27 20:55:30 +0800

    qmk to use IGNORE_MOD_TAP_INTERRUPT by default

--- .config/qmk/nguwijy/config.h
+++ .config/qmk/nguwijy/config.h
@@ -1,9 +1,12 @@
 /* miliseconds for a key to become mod (used in tapping mode) */
-/* #define TAPPING_TERM 200 */
+#define TAPPING_TERM 200
 
 /* tapping instead of holding when no additional keystroke is pressed */
-#define RETRO_TAPPING
+/* #define RETRO_TAPPING */
 
 /* by default, holding the same key after tapping it is interpreted as tapping */
 /* such behavior is turned off by defining TAPPING_FORCE_HOLD */
 #define TAPPING_FORCE_HOLD
+
+/* hold only when both keys are held longer than TAPPING_TERM */
+#define IGNORE_MOD_TAP_INTERRUPT
