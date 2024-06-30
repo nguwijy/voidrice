1cc42f61c06480bd7563d414e900b29140272f31
nguwijy <nguwijy@hotmail.com>
2024-06-30 13:44:26 +0800

    add scripts for alpha research

--- example.py
+++ example.py
@@ -1,38 +1,69 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+################ data #################
+import akshare as ak
+import pandas as pd
+import tqdm
+
+res = []
+
+tmp = ak.stock_us_spot_em().rename(columns={'代码': 'symbol'})[['symbol']]
+tmp['ticker'] = tmp['symbol'].str.split('.').str[1]
+universe = tmp[tmp['ticker'].isin(pd.read_csv('index/spx.csv')['Symbol'].to_list())]['symbol'].to_list()
+
+for symbol in tqdm.tqdm(universe):
+    df = (
+        ak.stock_us_hist(symbol=symbol, period="daily", adjust="qfq")
+        .rename(columns={
+            "日期": "date",
+            "开盘": "open",
+            "收盘": "close",
+            "最高": "high",
+            "最低": "low",
+            "成交量": "volume",
+            "成交额": "amount",
+            "涨跌幅": "pctChg",
+            "换手率": "turnover"
+        })
+        .drop(columns=['振幅', '涨跌额'])
+    ).merge(
+        ak.stock_us_hist(symbol=symbol, period="daily", adjust="")
+        .rename(columns={"日期": "date", '收盘': 'close_unadjusted'})
+        [['date', 'close_unadjusted']]
+    )
+    df['inst'] = symbol
+    res.append(df)
+pd.concat(res, ignore_index=True).to_csv('data_qfq/us_top500.csv', index=False)
 
 
-# You need both chromedriver and chrome for this to work
-# For chrome, you can download easily from software center
-# For chromedriver, maybe this is something to be checked with IT, but the links are below:
-# https://googlechromelabs.github.io/chrome-for-testing/
-# https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.60/linux64/chromedriver-linux64.zip
-# https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.60/win64/chromedriver-win64.zip
-CHROME_DRIVER_PATH = '/home/nguwijy/Downloads/chromedriver-linux64/chromedriver'
-CHROME_PATH        = '/usr/bin/google-chrome-stable'
-DOWNLOAD_PATH      = '/home/nguwijy/'
-TARGET_URL         = 'https://eservices.mas.gov.sg/statistics/fdanet/BondPricesAndYields.aspx'
+################ gen alphas #################
+import pandas as pd
+import numpy as np
+import tqdm
+df = pd.read_csv('data_qfq/us_top500.csv')
+df = df[~np.isclose(df['amount'], 0)]
+df['vwap'] = df['amount'] / df['volume'] / df['close_unadjusted'] * df['close']
+df = df.drop_duplicates(subset=['date', 'inst']).pivot(index='date', columns='inst')
+alphas = Alphas101(df)
+methods = alphas.get_alpha_methods(alphas)
 
-# parse the relevant paths to the driver
-options = webdriver.ChromeOptions()
-options.binary_location = CHROME_PATH
-prefs = {'download.default_directory': DOWNLOAD_PATH, 'directory_upgrade': True}
-options.add_experimental_option('prefs', prefs)
-service = webdriver.ChromeService(executable_path=CHROME_DRIVER_PATH)
-driver = webdriver.Chrome(service=service, options=options)
+aa = pd.DataFrame()
+for m in tqdm.tqdm(methods):
+    factor = getattr(alphas, m)
+    if aa.empty:
+        aa = factor().stack().to_frame(m).reset_index()
+    else:
+        aa = aa.merge(factor().stack().to_frame(m).reset_index(), how='outer')
+aa.to_pickle('data_qfq/alpha_us_top500.pkl')
 
-# access the url
-driver.get(TARGET_URL)
+import pandas as pd
+from sklearn.feature_selection import mutual_info_regression
 
-# change the dropdown values
-# others dropdowns include
-# OptionSelect, ContentPlaceHolder1_StartMonthDropDownList, ContentPlaceHolder1_EndYearDropDownList
-# ContentPlaceHolder1_EndMonthDropDownList, ContentPlaceHolder1_FrequencyDropDownList
-select = Select(driver.find_elements(value='ContentPlaceHolder1_StartYearDropDownList')[0])
-select.select_by_visible_text('2021')
+alphas = pd.read_pickle('data_qfq/alpha_us_top500.pkl').merge(
+    pd.read_csv('data_qfq/us_top500.csv')
+    .drop_duplicates(subset=['date', 'inst'])
+    .pivot(index='date', columns='inst', values='close')
+    .pct_change(1).shift(-1).stack().to_frame('c2c').reset_index()
+).drop_duplicates(['date', 'inst']).dropna().set_index(['date', 'inst'])
+cols = list(set(alphas.columns) - {'alpha084', 'c2c'})
 
-# Select all and download!
-driver.find_elements(By.XPATH, "//*[contains(text(), 'Select All')]")[0].click()
-driver.find_element(value='ContentPlaceHolder1_Button2').click()
-driver.close()
+correl = alphas[cols].corrwith(alphas['c2c'], method='spearman')
+mi = mutual_info_regression(alphas[cols], alphas['c2c'])
